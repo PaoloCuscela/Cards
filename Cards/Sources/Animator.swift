73730cbac08c9cce31ebe611c3be681e36f1c122
Paolo <cuscio.2@gmail.com>
2017-10-27 10:03:02 +0200

    New Detail Presentation System + Parallax
     - Detail view presentation uses a ViewController and implements "UIViewControllerTransitioningDelegate", so presentation now works even when cards are in a scrollView.
    - Add parallax effect to cards
    - Fixed minor bugs
    - Code cleanup

---
+++ Cards/Sources/Animator.swift
@@ -0,0 +1,132 @@
+//
+//  Animator.swift
+//  Cards
+//
+//  Created by Paolo Cuscela on 23/10/17.
+//
+
+import UIKit
+
+class Animator: NSObject, UIViewControllerAnimatedTransitioning {
+
+    
+    fileprivate var presenting: Bool
+    fileprivate var velocity = 0.6
+    var bounceIntensity: CGFloat = 0.1
+    var card: Card
+    
+    init(presenting: Bool, from card: Card) {
+        self.presenting = presenting
+        self.card = card
+        super.init()
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        // Animation Context Setup
+        let container = transitionContext.containerView
+        let to = transitionContext.viewController(forKey: .to)!
+        let from = transitionContext.viewController(forKey: .from)!
+        container.addSubview(to.view)
+        container.addSubview(from.view)
+        
+        
+        guard presenting else {
+            
+            // Detail View Controller Dismiss Animations
+            
+            let superVC = to
+            let detailVC = from as! DetailViewController
+            let cardBackgroundFrame = detailVC.scrollView.convert(detailVC.cardBackground.frame, to: nil)
+            let bounce = self.bounceTransform(cardBackgroundFrame, to: card.originalFrame)
+            
+            // Blur and fade with completion
+            UIView.animate(withDuration: velocity, delay: 0, options: .curveEaseOut, animations: {
+                
+                detailVC.blurView.alpha = 0
+                detailVC.snap.alpha = 0
+                
+            }, completion: { _ in
+                
+                self.card.addSubview(detailVC.cardBackground)
+                transitionContext.completeTransition(true)
+            })
+            
+            // Layout with bounce effect
+            UIView.animate(withDuration: velocity/2, delay: 0, options: .curveEaseOut, animations: {
+                
+                detailVC.layout(self.card.originalFrame, isPresenting: false, transform: bounce)
+                self.card.delegate?.cardIsHidingDetail?(card: self.card)
+                
+            }) { _ in UIView.animate(withDuration: self.velocity/2, delay: 0, options: .curveEaseOut, animations: {
+                    
+                detailVC.layout(self.card.originalFrame, isPresenting: false)
+                self.card.delegate?.cardIsHidingDetail?(card: self.card)
+                })
+            }
+            return
+
+        }
+        
+        // Detail View Controller Present Animations
+        
+        let detailVC = to as! DetailViewController
+        let bounce = self.bounceTransform(card.originalFrame, to: UIScreen.main.bounds)
+        
+        container.bringSubview(toFront: detailVC.view)
+        detailVC.cardBackground = card.backgroundIV
+        detailVC.layout(card.originalFrame, isPresenting: false)
+        
+        // Blur and fade with completion
+        UIView.animate(withDuration: velocity, delay: 0, options: .curveEaseOut, animations: {
+            
+            self.card.transform = CGAffineTransform.identity    // Reset card identity after push back on tap
+            detailVC.blurView.alpha = 1
+            detailVC.snap.alpha = 1
+            
+        }, completion: { _ in
+            
+            transitionContext.completeTransition(true)
+        })
+        
+        // Layout with bounce effect
+        UIView.animate(withDuration: velocity/2, delay: 0, options: .curveEaseOut, animations: {
+            
+            detailVC.layout(detailVC.view.frame, isPresenting: true, transform: bounce)
+            self.card.delegate?.cardIsShowingDetail?(card: self.card)
+            
+        }) { _ in UIView.animate(withDuration: self.velocity/2, delay: 0, options: .curveEaseOut, animations: {
+                
+            detailVC.layout(detailVC.view.frame, isPresenting: true)
+            self.card.delegate?.cardIsShowingDetail?(card: self.card)
+                
+            })
+        }
+        
+    }
+    
+    private func bounceTransform(_ from: CGRect, to: CGRect ) -> CGAffineTransform {
+        
+        let old = from.center
+        let new = to.center
+        
+        let xDistance = old.x - new.x
+        let yDistance = old.y - new.y
+        
+        let xMove = -( xDistance * bounceIntensity )
+        let yMove = -( yDistance * bounceIntensity )
+        
+        //let xMove = (old.x < new.x ) ? LayoutHelper.XScreen(bounceIntensity) : -LayoutHelper.XScreen(bounceIntensity)
+        //let yMove = (old.y < new.y ) ? LayoutHelper.YScreen(bounceIntensity) : -LayoutHelper.YScreen(bounceIntensity)
+        
+        return CGAffineTransform(translationX: xMove, y: yMove)
+    }
+
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return velocity
+    }
+    
+}
+
+
---
+++ Cards/Sources/DetailViewController.swift
@@ -0,0 +1,137 @@
+//
+//  DetailViewController.swift
+//  Cards
+//
+//  Created by Paolo Cuscela on 23/10/17.
+//
+
+import UIKit
+
+internal class DetailViewController: UIViewController {
+    
+    var blurView = UIVisualEffectView(effect: UIBlurEffect(style: .extraLight ))
+    var detailView: UIView?
+    var scrollView = UIScrollView()
+    var originalFrame = CGRect.zero
+    var snap = UIView()
+    var cardBackground: UIView! {
+        didSet{
+            scrollView.addSubview(cardBackground)
+        }
+    }
+    
+    
+    //MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.snap = UIScreen.main.snapshotView(afterScreenUpdates: true)
+        self.view.addSubview(blurView)
+        self.view.addSubview(scrollView)
+        if let detail = detailView {
+            
+            scrollView.addSubview(detail)
+            detail.alpha = 0
+        }
+        
+        blurView.frame = self.view.bounds
+        
+        scrollView.layer.backgroundColor = UIColor.white.cgColor
+        scrollView.layer.cornerRadius = 20
+        
+        scrollView.delegate = self
+        scrollView.alwaysBounceVertical = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.view.insertSubview(snap, belowSubview: blurView)
+        originalFrame = scrollView.frame
+        
+        if let detail = detailView {
+            
+            detail.alpha = 1
+            detail.frame = CGRect(x: 0,
+                                  y: cardBackground.bounds.maxY,
+                                  width: scrollView.frame.width,
+                                  height: detail.frame.height)
+            
+            scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: detail.frame.maxY)
+        }
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        snap.removeFromSuperview()
+    }
+    
+    
+    //MARK: - Layout & Animations for the content ( rect = Scrollview + card + detail )
+    
+    func layout(_ rect: CGRect, isPresenting: Bool, transform: CGAffineTransform = CGAffineTransform.identity){
+        
+        guard isPresenting else {
+            
+            scrollView.frame = rect.applying(transform)
+            cardBackground.frame = scrollView.bounds
+            return
+        }
+        
+        scrollView.frame.size = CGSize(width: LayoutHelper.XScreen(85), height: LayoutHelper.YScreen(100) - 20)
+        scrollView.center = blurView.center
+        scrollView.frame.origin.y = 40
+        scrollView.frame = scrollView.frame.applying(transform)
+        
+        cardBackground.frame.origin = scrollView.bounds.origin
+        cardBackground.frame.size = CGSize( width: scrollView.bounds.width,
+                                            height: cardBackground.bounds.height)
+    }
+}
+
+
+//MARK: - ScrollView Behaviour
+
+extension DetailViewController: UIScrollViewDelegate {
+    
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let y = scrollView.contentOffset.y
+        let origin = originalFrame.origin.y
+        let currentOrigin = originalFrame.origin.y
+        
+        if (y<0  || currentOrigin > origin) {
+            scrollView.frame.origin.y -= y/2
+            scrollView.contentOffset.y = 0
+        }
+        
+        //card.delegate?.cardDetailIsScrolling?(card: card)
+    }
+    
+    public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        let origin = originalFrame.origin.y
+        let currentOrigin = scrollView.frame.origin.y
+        let max = 4.0
+        let min = 2.0
+        var speed = Double(-velocity.y)
+        
+        if speed > max { speed = max }
+        if speed < min { speed = min }
+        
+        //self.bounceIntensity = CGFloat(speed-1)
+        speed = (max/speed*min)/10
+        
+        guard (currentOrigin - origin) < 60 else { dismiss(animated: true, completion: nil); return }
+        UIView.animate(withDuration: speed) { scrollView.frame.origin.y = self.originalFrame.origin.y }
+    }
+    
+    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        UIView.animate(withDuration: 0.1) { scrollView.frame.origin.y = self.originalFrame.origin.y }
+    }
+    
+}
+
+
